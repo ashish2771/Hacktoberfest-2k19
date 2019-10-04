fc7f709f34f4a61585a2aa6e43da9f20188f8a3a
ashish2771 <56157565+ashish2771@users.noreply.github.com>
2019-10-04 16:50:46 +0530

    length of word.c
    It tells the length of word given as input

---
+++ length of word.c
@@ -0,0 +1,13 @@
+// this program counts the number of elements in the word
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i;
+    char a[500];
+    printf("enter the string\n");
+    scanf("%s",a);
+    for (i=0;a[i] != '\0';i++)
+    {}
+    printf("the number of elements in the word is %d\n",i);
+}
